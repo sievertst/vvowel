d19eff2193a15c64332b99897642d61e174c5fbc
sievertst <sievert.stuelpnagel@mail.de>
2024-07-26 16:39:42 +0200

    Use flake-parts instead of flake-utils
    Also add formatter

--- flake.nix
+++ flake.nix
@@ -3,33 +3,47 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixfmt.url = "github:NixOS/nixfmt";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: 
-    flake-utils.lib.eachDefaultSystem (system:
-    let 
-      pkgs = nixpkgs.legacyPackages.${system};
-      pname = "vvowel";
-    in
+  outputs =
     {
-      packages.default = pkgs.stdenvNoCC.mkDerivation rec {
-        inherit pname;
-        version = "0.1";
-        outputs = [ "tex" ];
-        src = self;
-        nativeBuildInputs = [
-          (pkgs.writeShellScript "force-tex-output.sh" ''
-            out="''${tex-}"
-          '')
-        ];
-        installPhase = ''
-          runHook preInstall
-          path="$tex/tex/latex/${pname}"
-          mkdir -p "$path"
-          cp *.{sty,cls,def,clo} "$path"
-          runHook postInstall
-        '';
-      };
-    });
+      self,
+      nixpkgs,
+      flake-parts,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      perSystem =
+        { pkgs, system, ... }:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          pname = "vvowel";
+        in
+        {
+          packages.default = pkgs.stdenvNoCC.mkDerivation rec {
+            inherit pname;
+            version = "0.1";
+            outputs = [ "tex" ];
+            src = self;
+            nativeBuildInputs = [
+              (pkgs.writeShellScript "force-tex-output.sh" ''
+                out="''${tex-}"
+              '')
+            ];
+            installPhase = ''
+              runHook preInstall
+              path="$tex/tex/latex/${pname}"
+              mkdir -p "$path"
+              cp *.{sty,cls,def,clo} "$path"
+              runHook postInstall
+            '';
+          };
+
+          formatter = inputs.nixfmt.packages.${system}.default;
+        };
+    };
 }
